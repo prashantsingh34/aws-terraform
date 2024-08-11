1d134ae1d20a72d8910438bad5a2a4e27369b321
Prashant <113302801+prashantsingh34@users.noreply.github.com>
2024-08-11 11:02:09 +0530

    Create test_handler.py

---
+++ python/test_handler.py
@@ -0,0 +1,10 @@
+# import requests
+import json
+def lambda_handler(event, context):
+    # TODO implement
+    # config_url='https://jsonplaceholder.typicode.com/todos'
+    # posts= requests.get(config_url)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
